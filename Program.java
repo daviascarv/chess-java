813165420f3faa7367d1ff501c2c3c82feb4d3a5
Davi Augusto <davi_augusto11@hotmail.com>
2022-01-20 14:58:57 -0300

    File program modified

--- Program.java
+++ Program.java
@@ -1,9 +1,16 @@
 package Jogo_de_Xadrez;
 import java.util.Date;
+import java.util.Locale;
+
+import Jogo_de_Xadrez.boardGame.Position;
 
 public class Program {
     public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        Locale.setDefault(new Locale("pt-br"));
+        Date now = new Date();
         System.out.println("Hello World!");
+
     }
     
 }
