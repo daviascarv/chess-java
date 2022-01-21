811498b9aa57cb91044f9eabf0f970a4e325787b
Davi Augusto <davi_augusto11@hotmail.com>
2022-01-21 11:32:39 -0300

    Board and piece added and modified

---
+++ boardGame/Board.java
@@ -0,0 +1,26 @@
+package Jogo_de_Xadrez.boardGame;
+
+public class Board {
+    private int rows;
+    private int colums;
+    private Piece[][] pieces;
+
+    public Board(int rows, int colums){
+        this.rows = rows;
+        this.colums = colums;
+        pieces = new Piece[rows][colums];
+    }
+    public int getColums() {
+        return colums;
+    }
+    public void setColums(int colums) {
+        this.colums = colums;
+    }
+    public int getRows() {
+        return rows;
+    }
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+}
---
+++ boardGame/Piece.java
@@ -0,0 +1,14 @@
+package Jogo_de_Xadrez.boardGame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+public Piece(Board board){
+    this.board = board;
+}
+    protected Board getBoard() {
+        return board;
+    }
+    
+}
