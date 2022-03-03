fada04d50d5e48ef469100734556b7f6cfe6ca09
Davi Augusto <davi_augusto11@hotmail.com>
2022-03-03 11:53:58 -0300

    Class chess match modified: consrtuctors and methods added

--- Chess/ChessMatch.java
+++ Chess/ChessMatch.java
@@ -1,5 +1,22 @@
 package Jogo_de_Xadrez.Chess;
 
+import Jogo_de_Xadrez.boardGame.Board;
+
 public class ChessMatch {
+    
+    private Board board;
+
+    public ChessMatch(){
+        board = new Board(8, 8);
+    }
+    public ChessPiece[][] getPiece(){
+        ChessPiece [][] mat = new ChessPiece[board.getRows()][board.getColums()];
+        for(int i = 0; i < board.getRows(); i++){
+            for(int j = 0; j < board.getColums(); j++){
+                mat[i][j] = (ChessPiece) board.piece(i, j);
+            }
+        }
+        return mat;
+    }
     
 }
